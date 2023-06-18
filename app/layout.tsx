c75febdf25809cdd48c9de5cf54a6e587113d74d
kseniia <kseniia@smartiot.cloud>
2023-06-18 19:17:15 +0300

    simplify

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavMenu></NavMenu>
+        <NavMenu />
         {children}
       </body>
     </html>
